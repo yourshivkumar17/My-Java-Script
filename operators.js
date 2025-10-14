4d8e6c010fb7a46dd7bb46f5a1d8d14ee6bffb7e
shiv <yourshivkumar17@gmail.com>
2025-10-14 09:41:05 +0530

    third commit

---
+++ operators.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 3;
+
+console.log("a + b =", a + b);   // Addition
+console.log("a - b =", a - b);   // Subtraction
+console.log("a * b =", a * b);   // Multiplication
+console.log("a / b =", a / b);   // Division
+console.log("a % b =", a % b);   // Modulus (Remainder)
+console.log("a ** b =", a ** b); // Exponentiation
+
+console.log("a-- =", a--);       // Post-decrement: print first, then decrement
+console.log("a-- =", a--);       // Post-decrement again
+console.log("Current value of a =", a); // Check final value
+console.log("--a =", --a);   //pre-decrement: decrement first,then print
+console.log("++a =", ++a);  //pre-increment: increment first,then print
