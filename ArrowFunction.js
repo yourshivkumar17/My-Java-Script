05e2ba8c2198ea326b037b81d86ae4256a6087df
shiv <yourshivkumar17@gmail.com>
2025-10-16 10:52:33 +0530

    first

---
+++ ArrowFunction.js
@@ -0,0 +1,2 @@
+let addThree = (a, b, c) => a + b + c;
+console.log(addThree(2, 5, 3));
--- functionWithPerameter.js
+++ functionWithPerameter.js
@@ -2,5 +2,5 @@ function greet(name) {
   console.log("Hello, " + name + "!");
 }
 
-greet("Shiv");  
+greet("Shiv");
 greet("Alice");  
