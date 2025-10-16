8c4660d07cd84db12bf4ed2e67a0a986fe9a4f80
shiv <yourshivkumar17@gmail.com>
2025-10-16 10:38:23 +0530

    first

---
+++ AddTwoNumUsingFunction.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+  return a + b;  // function returns the sum of a and b where function is called
+}
+
+let result = add(5, 3);// function called with arguments 5 and 3(Addition)
+console.log(result);
