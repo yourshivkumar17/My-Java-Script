5ac8619e441ac8af3b9e372293ab061220351ce0
Shiv Kumar <yourshivkumar17@gmail.com>
2025-11-03 09:52:03 +0530

    Update console log statements with new syntax

---
+++ NewSyntex.js
@@ -0,0 +1,6 @@
+const name = "Riya";
+const birthYear = 2004;
+const age = 2025 - birthYear;
+
+console.log("Hello, my name is " + name + " and I am " + age + " years old.");  // old Syntex
+console.log(`Hello, my name is ${name} and I am ${age} years old.`);  // New Syntex
