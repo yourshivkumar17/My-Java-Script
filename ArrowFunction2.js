d5e15ef89d2760b878ab705d6ea1763bb5e9d15f
Shiv Kumar <yourshivkumar17@gmail.com>
2025-11-03 09:23:51 +0530

    Add ArrowFunction2.js with greeting and math functions

---
+++ ArrowFunction2.js
@@ -0,0 +1,10 @@
+const greet = (name) => "Hello, " + name + "!";
+
+const add = (a, b) => a + b;
+
+const isEven = (num) => num % 2 === 0;
+
+console.log(greet("Shiv")); 
+console.log(add(3, 4));   
+console.log(isEven(10));    
+
